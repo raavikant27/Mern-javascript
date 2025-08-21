4176c465ea2a929888ad6010aecc2c7fb3ec008f
Ravikant singh <sravikant302@gmail.com>
2025-08-21 12:57:53 +0530

    Async- Await -basic -Creation

--- Async-Await/script.js
+++ Async-Await/script.js
@@ -29,15 +29,36 @@
 
 //2n example 
 //creating a promise 
-const p =new Promise((resolve,reject)=>{
+// const p =new Promise((resolve,reject)=>{
+//     resolve("Promise Resolved Value");
+// });
+// async function getData(){
+//     //i need promise here return
+//     return p;
+// }
+
+// const dataPromise=getData();
+// dataPromise.then(
+//     (res)=>console.log(res)
+// );
+
+//notes-let see how we use asyn function along with await , nd how will handle actual Promise
+const p=new Promise((resolve,reject)=>{
     resolve("Promise Resolved Value");
 });
-async function getData(){
-    //i need promise here return
-    return p;
-}
 
-const dataPromise=getData();
-dataPromise.then(
-    (res)=>console.log(res)
-);
+async function handlePromise(){
+   
+ const val = await p;
+   
+console.log(val);
+}
+handlePromise();
+//call the handle Promise here
+
+
+
+// function getData(){
+//     p.then((res)=>console.log(res));
+// }
+// getData();
